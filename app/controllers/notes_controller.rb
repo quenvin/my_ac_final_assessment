e84fcfc1cb506215b0155511f2937199a4647a6a
quenvin <quenvint@hotmail.com>
2018-03-26 11:16:23 +0800

    Added top_nav with notes path and user login/signup

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,11 @@
 class NotesController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
     @notes = Note.all
+  end
+
+  def new
+    @note = Note.new
   end
 end
