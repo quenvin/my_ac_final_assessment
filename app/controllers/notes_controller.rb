de3e7d9f484d51370207cbe6e423ca69e2e5451c
quenvin <quenvint@hotmail.com>
2018-03-26 11:36:25 +0800

    Added create action in note controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -8,4 +8,16 @@ class NotesController < ApplicationController
   def new
     @note = Note.new
   end
+
+  def create
+    note = current_user.notes.new(note_params)
+    note.save
+    redirect_to notes_path
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :body)
+  end
 end
