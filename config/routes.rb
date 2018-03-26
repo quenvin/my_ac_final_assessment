d19b0a2d3c121722708398b34ebfc373646cfbfb
quenvin <quenvint@hotmail.com>
2018-03-26 10:37:41 +0800

    Added rotues for notes, like and follow

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :notes do
+    post 'like', on: :member, to: 'likes#create'
+    delete 'unlike', on: :member, to: 'likes#destroy'
+    post 'follow', on: :member, to: 'follows#create'
+    delete 'unfollow', on: :member, to: 'follows#destroy'
+  end
 end
