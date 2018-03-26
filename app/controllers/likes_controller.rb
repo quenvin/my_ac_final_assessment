d3496572ee310656cf5ec58fa3213b3beb7c6986
quenvin <quenvint@hotmail.com>
2018-03-26 18:10:27 +0800

    Added mailer for like and unlike

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,8 @@
 class LikesController < ApplicationController
   def create
     Like.create(note_id: params[:id], user: current_user)
+    like = Like.last
+    UserMailer.note_like_email(like).deliver_later 
     @notes = Note.all.order("created_at DESC")
     @users = User.all.order("username ASC")
     if current_user
@@ -13,6 +15,7 @@ class LikesController < ApplicationController
 
   def destroy
     like = Like.find_by(note_id:params[:id])
+    UserMailer.note_unlike_email(like).deliver_later 
     like.destroy
     @notes = Note.all.order("created_at DESC")
     @users = User.all.order("username ASC")
