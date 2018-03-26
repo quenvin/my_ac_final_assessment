9de276a8ea9452165e55c440ca523c72919fda1e
quenvin <quenvint@hotmail.com>
2018-03-26 12:21:52 +0800

    Added likes controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,22 @@
+class LikesController < ApplicationController
+  def create
+    Like.create(post_id: params[:id], user: current_user)
+    @notes = Note.all.order("created_at DESC")
+    render :toggle
+  end
+
+
+
+  def destroy
+    like = Like.find_by(post_id:params[:id])
+    like.destroy
+    @notes = Note.all.order("created_at DESC")
+    render :toggle
+  end
+
+  private
+
+  def likes_params
+    params.require(:like).permit(:note_id, :user_id)
+  end
+end
